3a142747e322a27d25e6baed6c8bff5a4981c6e0
github <129170172+Ebicrone@users.noreply.github.com>
2025-10-14 12:46:09 +0800

    Update
    Additions to storieslist, storycard, and newsfeed.

--- facebook/lib/pages/newsfeed.dart
+++ facebook/lib/pages/newsfeed.dart
@@ -1,4 +1,6 @@
+import 'package:facebook/model/story_model.dart';
 import 'package:facebook/widgets/createpost.dart';
+import 'package:facebook/widgets/storieslist.dart';
 import 'package:flutter/material.dart';
 
 class Newsfeed extends StatefulWidget {
@@ -9,20 +11,69 @@ class Newsfeed extends StatefulWidget {
 }
 
 class _NewsfeedState extends State<Newsfeed> {
+  List<StoryModel> stories = [
+
+    StoryModel(
+      id: "1", 
+      username: "Juan", profileImagePath: "assets/profile/prof1.jpg", 
+      storyImagePath: "assets/myday/myday1.jpg", 
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)), 
+      isViewed: false
+      ),
+
+      StoryModel(
+      id: "2", 
+      username: "Clara", profileImagePath: "assets/profile/prof2.jpg", 
+      storyImagePath: "assets/myday/myday2.jpg", 
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)), 
+      isViewed: false
+      ),
+
+      StoryModel(
+      id: "3", 
+      username: "Maria", profileImagePath: "assets/profile/prof3.jpg", 
+      storyImagePath: "assets/myday/myday3.jpg", 
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)), 
+      isViewed: false
+      ),
+
+      StoryModel(
+      id: "4", 
+      username: "Roberto", profileImagePath: "assets/profile/prof4.jpg", 
+      storyImagePath: "assets/myday/myday4.jpg", 
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)), 
+      isViewed: false
+      ),
+
+      StoryModel(
+      id: "5", 
+      username: "Alfonso", profileImagePath: "assets/profile/prof5.jpg", 
+      storyImagePath: "assets/myday/myday5.jpg", 
+      timestamp: DateTime.now().subtract(const Duration(hours: 5)), 
+      isViewed: false
+      ),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Image.asset("assets/icon/logo.png", height: 80,),
+        title: Image.asset("assets/icon/logo.png", height: 80),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
           DrawerButton(),
         ],
       ),
-      body: Column(
-        children: [Createpost(onpostcreate: (){})],
-      )
+      body: Column(children: [
+          const Createpost(),
+          StoriesList(
+            stories: stories,
+            onStoryTap: (story) {},
+          ),
+        ],
+      ),
     );
   }
 }
